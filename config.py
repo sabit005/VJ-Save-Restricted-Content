66bdd005bca8b4382f0794cf3c81dc93be23dbea
sabit005 <138222152+sabit005@users.noreply.github.com>
2025-03-05 12:48:17 +0600

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ ADMINS = int(os.environ.get("ADMINS", "6092149168"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://sabitsgaming1:<db_password>@cluster0.803f7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
